0c89fcb6c3bd65a711766176dc322633334c08cf
Penguin <vanesscapuras@gmail.com>
2024-10-21 11:13:58 +0800

    added the database

--- basicCrud/settings.py
+++ basicCrud/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'basicCrud.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'dbrecipeplatform',
+        'USER': 'root',
+        'PASSWORD': 'vanessgwapo',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
